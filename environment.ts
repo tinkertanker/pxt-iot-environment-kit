cf3591f30cf7dc5befff2335cfdc10b4db6c847e
ElecFreaks <support@elecfreaks.com>
2018-11-05 15:22:57 +0800

    Update environment.ts

--- environment.ts
+++ environment.ts
@@ -230,9 +230,9 @@ namespace Environment_IoT {
 
     /** 
     * TODO: get noise(dB)
-    * @param noisepin describe parameter here, eg: AnalogPin.P4
+    * @param noisepin describe parameter here, eg: AnalogPin.P1
     */
-    //% blockId="readnoise" block="read noise(dB) at pin %noisepin"
+    //% blockId="readnoise" block="value of noise(dB) at pin %noisepin"
     export function ReadNoise(noisepin: AnalogPin): number {
         let level = 0
         let voltage = 0
@@ -266,41 +266,82 @@ namespace Environment_IoT {
             suml = suml / l
         }
         noise = sumh - suml
-        if (noise <= 28) {
+        if (noise <= 4) {
             noise = pins.map(
                 noise,
                 0,
-                28,
-                15,
+                4,
+                30,
+                50
+            )
+        } else if (noise <= 8) {
+            noise = pins.map(
+                noise,
+                4,
+                8,
+                50,
                 55
             )
-        } else if (noise <= 70) {
+        } else if (noise <= 14) {
             noise = pins.map(
                 noise,
-                28,
-                70,
+                9,
+                14,
                 55,
-                64
+                60
             )
-        } else if (noise <= 229) {
+        } else if (noise <= 32) {
             noise = pins.map(
                 noise,
+                15,
+                32,
+                60,
+                70
+            )
+        } else if (noise <= 60) {
+            noise = pins.map(
+                noise,
+                33,
+                60,
                 70,
-                229,
-                64,
-                76
+                75
+            )
+        } else if (noise <= 100) {
+            noise = pins.map(
+                noise,
+                61,
+                100,
+                75,
+                80
+            )
+        } else if (noise <= 150) {
+            noise = pins.map(
+                noise,
+                101,
+                150,
+                80,
+                85
+            )
+        } else if (noise <= 231) {
+            noise = pins.map(
+                noise,
+                151,
+                231,
+                85,
+                90
             )
         } else {
             noise = pins.map(
                 noise,
-                229,
+                231,
                 1023,
-                76,
+                90,
                 120
             )
         }
-        return noise;
-    } 
+        noise = Math.round(noise)
+        return noise
+    }
 
 
 }
