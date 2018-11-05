7902213dc6021477fc16fb6387fc1314dacc2119
ElecFreaks <support@elecfreaks.com>
2018-11-05 15:41:37 +0800

    Update environment.ts

--- environment.ts
+++ environment.ts
@@ -19,7 +19,7 @@ enum DHT11Type {
 /**
  * Custom blocks
  */
-//% weight=90 color=#0fbc11 icon="\uf0ee"
+//% weight=90 color=#0fbc11 icon="\uf6c4"
 namespace Environment {
     
     let Reference_VOLTAGE = 3100
