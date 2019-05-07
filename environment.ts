2ed9dbbf871abcc44a1338f14defe747f0927ac3
LION-YAN\Lion <Lion_yan@outlook.com>
2019-05-07 11:46:37 +0800

    no message

--- environment.ts
+++ environment.ts
@@ -7,6 +7,47 @@
 namespace Environment {
 
 
+    let Reference_VOLTAGE = 3100
+    //BME280
+    let BME280_I2C_ADDR = 0x76
+    let dig_T1 = getUInt16LE(0x88)
+    let dig_T2 = getInt16LE(0x8A)
+    let dig_T3 = getInt16LE(0x8C)
+    let dig_P1 = getUInt16LE(0x8E)
+    let dig_P2 = getInt16LE(0x90)
+    let dig_P3 = getInt16LE(0x92)
+    let dig_P4 = getInt16LE(0x94)
+    let dig_P5 = getInt16LE(0x96)
+    let dig_P6 = getInt16LE(0x98)
+    let dig_P7 = getInt16LE(0x9A)
+    let dig_P8 = getInt16LE(0x9C)
+    let dig_P9 = getInt16LE(0x9E)
+    let dig_H1 = getreg(0xA1)
+    let dig_H2 = getInt16LE(0xE1)
+    let dig_H3 = getreg(0xE3)
+    let a = getreg(0xE5)
+    let dig_H4 = (getreg(0xE4) << 4) + (a % 16)
+    let dig_H5 = (getreg(0xE6) << 4) + (a >> 4)
+    let dig_H6 = getInt8LE(0xE7)
+    let T = 0
+    let P = 0
+    let H = 0
+    setreg(0xF2, 0x04)
+    setreg(0xF4, 0x2F)
+    setreg(0xF5, 0x0C)
+    setreg(0xF4, 0x2F)
+    //DS1307RTC
+    let DS1307_I2C_ADDR = 0x68;
+    let DS1307_REG_SECOND = 0
+    let DS1307_REG_MINUTE = 1
+    let DS1307_REG_HOUR = 2
+    let DS1307_REG_WEEKDAY = 3
+    let DS1307_REG_DAY = 4
+    let DS1307_REG_MONTH = 5
+    let DS1307_REG_YEAR = 6
+    let DS1307_REG_CTRL = 7
+    let DS1307_REG_RAM = 8
+
     export enum DHT11Type {
         //% block="temperature(℃)" enumval=0
         DHT11_temperature_C,
@@ -31,22 +72,92 @@ namespace Environment {
     }
 
 
-    export enum BME280Type {
-        //% block="temperature(℃)" enumval=0
-        BME280_temperature_C,
+    export enum BME280_state {
+        //% block="Temperature(℃)" enumval=0
+        Temperature,
+        //% block="Humidity(RH)" enumval=1
+        Humidity,
+        //% block="Pressure(Pa)" enumval=2
+        Pressure,
+        //% block="Altitude(M)" enumval=3
+        Altitude
+    }
 
-        //% block="humidity(0~100)" enumval=1
-        BME280_humidity,
+    function setreg(reg: number, dat: number): void {
+        let buf = pins.createBuffer(2);
+        buf[0] = reg;
+        buf[1] = dat;
+        pins.i2cWriteBuffer(BME280_I2C_ADDR, buf);
+    }
 
-        //% block="pressure(hPa)" enumval=2
-        BME280_pressure,
+    function getreg(reg: number): number {
+        pins.i2cWriteNumber(BME280_I2C_ADDR, reg, NumberFormat.UInt8BE);
+        return pins.i2cReadNumber(BME280_I2C_ADDR, NumberFormat.UInt8BE);
+    }
 
-        //% block="altitude(M)" enumval=3
-        BME280_altitude,
+    function getInt8LE(reg: number): number {
+        pins.i2cWriteNumber(BME280_I2C_ADDR, reg, NumberFormat.UInt8BE);
+        return pins.i2cReadNumber(BME280_I2C_ADDR, NumberFormat.Int8LE);
+    }
+
+    function getUInt16LE(reg: number): number {
+        pins.i2cWriteNumber(BME280_I2C_ADDR, reg, NumberFormat.UInt8BE);
+        return pins.i2cReadNumber(BME280_I2C_ADDR, NumberFormat.UInt16LE);
+    }
+
+    function getInt16LE(reg: number): number {
+        pins.i2cWriteNumber(BME280_I2C_ADDR, reg, NumberFormat.UInt8BE);
+        return pins.i2cReadNumber(BME280_I2C_ADDR, NumberFormat.Int16LE);
+    }
+    function get(): void {
+        let adc_T = (getreg(0xFA) << 12) + (getreg(0xFB) << 4) + (getreg(0xFC) >> 4)
+        let var1 = (((adc_T >> 3) - (dig_T1 << 1)) * dig_T2) >> 11
+        let var2 = (((((adc_T >> 4) - dig_T1) * ((adc_T >> 4) - dig_T1)) >> 12) * dig_T3) >> 14
+        let t = var1 + var2
+        T = ((t * 5 + 128) >> 8) / 100
+        var1 = (t >> 1) - 64000
+        var2 = (((var1 >> 2) * (var1 >> 2)) >> 11) * dig_P6
+        var2 = var2 + ((var1 * dig_P5) << 1)
+        var2 = (var2 >> 2) + (dig_P4 << 16)
+        var1 = (((dig_P3 * ((var1 >> 2) * (var1 >> 2)) >> 13) >> 3) + (((dig_P2) * var1) >> 1)) >> 18
+        var1 = ((32768 + var1) * dig_P1) >> 15
+        if (var1 == 0)
+            return; // avoid exception caused by division by zero
+        let adc_P = (getreg(0xF7) << 12) + (getreg(0xF8) << 4) + (getreg(0xF9) >> 4)
+        let _p = ((1048576 - adc_P) - (var2 >> 12)) * 3125
+        _p = (_p / var1) * 2;
+        var1 = (dig_P9 * (((_p >> 3) * (_p >> 3)) >> 13)) >> 12
+        var2 = (((_p >> 2)) * dig_P8) >> 13
+        P = _p + ((var1 + var2 + dig_P7) >> 4)
+        let adc_H = (getreg(0xFD) << 8) + getreg(0xFE)
+        var1 = t - 76800
+        var2 = (((adc_H << 14) - (dig_H4 << 20) - (dig_H5 * var1)) + 16384) >> 15
+        var1 = var2 * (((((((var1 * dig_H6) >> 10) * (((var1 * dig_H3) >> 11) + 32768)) >> 10) + 2097152) * dig_H2 + 8192) >> 14)
+        var2 = var1 - (((((var1 >> 15) * (var1 >> 15)) >> 7) * dig_H1) >> 4)
+        if (var2 < 0) var2 = 0
+        if (var2 > 419430400) var2 = 419430400
+        H = (var2 >> 12) / 1024
+    }
+    ///////////////////////////rtc1307
+    function RTC_setReg(reg: number, dat: number): void {
+        let buf = pins.createBuffer(2);
+        buf[0] = reg;
+        buf[1] = dat;
+        pins.i2cWriteBuffer(DS1307_I2C_ADDR, buf);
+    }
+    function RTC_getReg(reg: number): number {
+        pins.i2cWriteNumber(DS1307_I2C_ADDR, reg, NumberFormat.UInt8BE);
+        return pins.i2cReadNumber(DS1307_I2C_ADDR, NumberFormat.UInt8BE);
+    }
+    function HexToDec(dat: number): number {
+        return (dat >> 4) * 10 + (dat % 16);
+    }
+    function DecToHex(dat: number): number {
+        return Math.idiv(dat, 10) * 16 + (dat % 10)
     }
 
 
-    let Reference_VOLTAGE = 3100
+
 
     /**
      * get dust value (μg/m³) 
@@ -448,6 +559,30 @@ namespace Environment {
         }
         noise = Math.round(noise)
         return Math.round(noise)
+    }
+    //% block="Read BME280 %state value"
+    export function octopus_BME280(state: BME280_state): number {
+        switch (state) {
+            case 0:
+                get();
+                return Math.round(T);
+                break;
+            case 1:
+                get();
+                return Math.round(H);
+                break;
+            case 2:
+                get();
+                return Math.round(P / 100);
+                break;
+            case 3:
+                get();
+                return (Math.round(P / 100) - 970) * 3
+                break;
+            default:
+                return 0
+        }
+        return 0;
     }
 
 }
