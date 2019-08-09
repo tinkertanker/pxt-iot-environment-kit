9ba8e765ad26a6fd76e381da3fe225d63a4acd82
LION-YAN\Lion <Lion_yan@outlook.com>
2019-08-09 11:36:52 +0800

    no message

--- tests.ts
+++ tests.ts
@@ -1,12 +1,21 @@
-// tests go here; this will not be compiled when this package is used as a library
-
-OLED.init(64, 128)
-OLED.showString("hello, world!")
-OLED.showString("counter:")
-let item = 0
-basic.forever(() => {
-    basic.pause(1000)
-    item += 1
-    OLED.showNumber(item)
+ESP8266_IoT.connectWifi(
+SerialPin.P8,
+SerialPin.P12,
+BaudRate.BaudRate115200,
+"your_ssid",
+"your_pw"
+)
+basic.forever(function () {
+    ESP8266_IoT.connectThingSpeak(
+    "api.thingspeak.com",
+    "your_write_api_key",
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0
+    )
 })
-
