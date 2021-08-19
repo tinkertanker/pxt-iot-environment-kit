91108f4120cf11611a1cdf6913e2fdb3635228ae
Marek Mansell <marek.mansell@gmail.com>
2021-08-20 00:31:19 +0200

    Corrected distance calculation formula

--- environment.ts
+++ environment.ts
@@ -300,7 +300,7 @@ let sc_byte = 0
 
         // read pulse
         let d = pins.pulseIn(pin, PulseValue.High, 23000)  // 8 / 340 = 
-        let distance = d * 10 * 5 / 3 / 58
+        let distance = d * 10 / 58
 
         if (distance > 4000) distance = 0
 
