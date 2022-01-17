b01f3c0ed4ca7a2ef9e5aa31bc37b8d66c8bf1d3
elecfreaks <support@elecfreaks.com>
2022-01-17 17:05:55 +0800

    Add RFID

--- environment.ts
+++ environment.ts
@@ -171,7 +171,123 @@ namespace Environment {
         if (var2 > 419430400) var2 = 419430400
         H = (var2 >> 12) / 1024
     }
-let sc_byte = 0
+
+    ////////////////////////////////////////////////////////NFC////////////////////////////////////////////
+    let NFC_I2C_ADDR = (0x48 >> 1);
+    let recvBuf = pins.createBuffer(32);
+    let recvAck = pins.createBuffer(8);
+    let ackBuf = pins.createBuffer(6);
+    let uId = pins.createBuffer(4);
+    let passwdBuf = pins.createBuffer(6);
+    let blockData = pins.createBuffer(16);
+    let NFC_ENABLE = 0;
+    const block_def = 1;
+    ackBuf[0] = 0x00;
+    ackBuf[1] = 0x00;
+    ackBuf[2] = 0xFF;
+    ackBuf[3] = 0x00;
+    ackBuf[4] = 0xFF;
+    ackBuf[5] = 0x00;
+    passwdBuf[0] = 0xFF;
+    passwdBuf[1] = 0xFF;
+    passwdBuf[2] = 0xFF;
+    passwdBuf[3] = 0xFF;
+    passwdBuf[4] = 0xFF;
+    passwdBuf[5] = 0xFF;
+    function writeAndReadBuf(buf: Buffer, len: number) {
+        pins.i2cWriteBuffer(NFC_I2C_ADDR, buf);
+        basic.pause(100);
+        recvAck = pins.i2cReadBuffer(NFC_I2C_ADDR, 8);
+        basic.pause(100);
+        recvBuf = pins.i2cReadBuffer(NFC_I2C_ADDR, len - 4);
+    }
+    function checkDcs(len: number): boolean {
+        let sum = 0, dcs = 0;
+        for (let i = 1; i < len - 2; i++) {
+            if ((i === 4) || (i === 5)) {
+                continue;
+            }
+            sum += recvBuf[i];
+        }
+        dcs = 0xFF - (sum & 0xFF);
+        if (dcs != recvBuf[len - 2]) {
+            return false;
+        }
+        return true;
+    }
+    function passwdCheck(id: Buffer, st: Buffer): boolean {
+        let buf: number[] = [];
+        buf = [0x00, 0x00, 0xFF, 0x0F, 0xF1, 0xD4, 0x40, 0x01, 0x60, 0x07, 0xFF,
+            0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xD1, 0xAA, 0x40, 0xEA, 0xC2, 0x00];
+        let cmdPassWord = pins.createBufferFromArray(buf);
+        let sum = 0, count = 0;
+        cmdPassWord[9] = block_def;
+        for (let i = 10; i < 16; i++)
+            cmdPassWord[i] = st[i - 10];
+        for (let i = 16; i < 20; i++)
+            cmdPassWord[i] = id[i - 16];
+        for (let i = 0; i < 20; i++) {
+            if (i === 3 || i === 4) {
+                continue;
+            }
+            sum += cmdPassWord[i];
+        }
+        cmdPassWord[20] = 0xff - (sum & 0xff)
+        writeAndReadBuf(cmdPassWord, 15);
+        for (let i = 0; i < 4; i++) {
+            if (recvAck[1 + i] != ackBuf[i]) {
+                serial.writeLine("psd ack ERROR!");
+                return false;
+            }
+        }
+        if ((recvBuf[6] === 0xD5) && (recvBuf[7] === 0x41) && (recvBuf[8] === 0x00) && (checkDcs(15 - 4))) {
+            return true;
+        }
+        return false;
+    }
+    function wakeup() {
+        basic.pause(100);
+        let i = 0;
+        let buf: number[] = [];
+        buf = [0x00, 0x00, 0xFF, 0x05, 0xFB, 0xD4, 0x14, 0x01, 0x14, 0x01, 0x02, 0x00];
+        let cmdWake = pins.createBufferFromArray(buf);
+        writeAndReadBuf(cmdWake, 14);
+        for (i = 0; i < ackBuf.length; i++) {
+            if (recvAck[1 + i] != ackBuf[i]) {
+                break;
+            }
+        }
+        if ((i != ackBuf.length) || (recvBuf[6] != 0xD5) || (recvBuf[7] != 0x15) || (!checkDcs(14 - 4))) {
+            NFC_ENABLE = 0;
+        } else {
+            NFC_ENABLE = 1;
+        }
+        basic.pause(100);
+    }
+    function writeblock(data: Buffer): void {
+        if (!passwdCheck(uId, passwdBuf))
+            return;
+        let cmdWrite: number[] = [0x00, 0x00, 0xff, 0x15, 0xEB, 0xD4, 0x40, 0x01, 0xA0,
+            0x06, 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07,
+            0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F, 0xCD,
+            0x00];
+        let sum = 0, count = 0;
+        cmdWrite[9] = block_def;
+        for (let i = 10; i < 26; i++)
+            cmdWrite[i] = data[i - 10];
+        for (let i = 0; i < 26; i++) {
+            if ((i === 3) || (i === 4)) {
+                continue;
+            }
+            sum += cmdWrite[i];
+        }
+        cmdWrite[26] = 0xff - (sum & 0xff);
+        let tempbuf = pins.createBufferFromArray(cmdWrite)
+        writeAndReadBuf(tempbuf, 16);
+    }
+    ///////////////////////////////////////////////////////////////////////////////////////////
+
+    let sc_byte = 0
     let dat = 0
     let low = 0
     let high = 0
@@ -329,7 +445,7 @@ let sc_byte = 0
     export function dht11value(dht11type: DHT11Type, dht11pin: DigitalPin): number {
 
         //initialize
-	basic.pause(1100)
+	    basic.pause(1100)
         let _temperature: number = -999.0
         let _humidity: number = -999.0
         let checksum: number = 0
@@ -695,5 +811,81 @@ let sc_byte = 0
             brightness = 0
         }
     }
+
+    //% block="RFID sensor IIC port read data from card"
+    export function readDataBlock(): string {
+        if (NFC_ENABLE === 0) {
+            wakeup();
+        }
+        if (checkCard() === false) {
+            serial.writeLine("No NFC Card!")
+            return ""
+        }
+        if (!passwdCheck(uId, passwdBuf)) {
+            serial.writeLine("passwd error!")
+            return "";
+        }
+        let cmdRead: number[] = []
+        cmdRead = [0x00, 0x00, 0xff, 0x05, 0xfb, 0xD4, 0x40, 0x01, 0x30, 0x07, 0xB4, 0x00];
+        let sum = 0, count = 0;
+        cmdRead[9] = block_def;
+        for (let i = 0; i < cmdRead.length - 2; i++) {
+            if ((i === 3) || (i === 4)) {
+                continue;
+            }
+            sum += cmdRead[i];
+        }
+        cmdRead[cmdRead.length - 2] = 0xff - sum & 0xff;
+        let buf = pins.createBufferFromArray(cmdRead)
+        writeAndReadBuf(buf, 31);
+        let ret = "";
+        if ((recvBuf[6] === 0xD5) && (recvBuf[7] === 0x41) && (recvBuf[8] === 0x00) && (checkDcs(31 - 4))) {
+            for (let i = 0; i < 16; i++) {
+                if (recvBuf[i + 9] >= 0x20 && recvBuf[i + 9] < 0x7f) {
+                    ret += String.fromCharCode(recvBuf[i + 9]) // valid ascii
+                }
+            }
+            return ret;
+        }
+        return ""
+    }
+    //% block="RFID sensor IIC port write %data to card"
+    export function writeData(data: string): void {
+        let len = data.length
+        if (len > 16) {
+            len = 16
+        }
+        for (let i = 0; i < len; i++) {
+            blockData[i] = data.charCodeAt(i)
+        }
+        writeblock(blockData);
+    }
+    //% block="RFID sensor IIC port Detect Card"
+    export function checkCard(): boolean {
+        if (NFC_ENABLE === 0) {
+            wakeup();
+        }
+        let buf: number[] = [];
+        buf = [0x00, 0x00, 0xFF, 0x04, 0xFC, 0xD4, 0x4A, 0x01, 0x00, 0xE1, 0x00];
+        let cmdUid = pins.createBufferFromArray(buf);
+        writeAndReadBuf(cmdUid, 24);
+        for (let i = 0; i < 4; i++) {
+            if (recvAck[1 + i] != ackBuf[i]) {
+                return false;
+            }
+        }
+        if ((recvBuf[6] != 0xD5) || (!checkDcs(24 - 4))) {
+            return false;
+        }
+        for (let i = 0; i < uId.length; i++) {
+            uId[i] = recvBuf[14 + i];
+        }
+        if (uId[0] === uId[1] && uId[1] === uId[2] && uId[2] === uId[3] && uId[3] === 0xFF) {
+            return false;
+        }
+        return true;
+    }
+
+
 }
 
