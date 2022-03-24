fe599857499981379b8412c5b85cdb812b821eda
ELECFREAKS.Admin <support@elecfreaks.com>
2022-03-24 16:50:43 +0800

    更新 pxt.json, environment.ts

--- environment.ts
+++ environment.ts
@@ -440,16 +440,61 @@ namespace Environment {
     let dht11_resultArray: number[] = []
     let dht11_pin = 0
 
-    function get_dht11value(){
+    // function get_dht11value(){
 
-        if(dht11_pin == 0){
+    //     if(dht11_pin == 0){
+    //         return
+    //     }
+
+    //     let checksum: number = 0
+    //     let checksumTmp: number = 0
+    //     let dataArray: boolean[] = []
+    //     let last_time:number = 0
+
+    //     for (let index = 0; index < 40; index++) dataArray.push(false)
+    //     for (let index = 0; index < 5; index++) dht11_resultArray.push(0)
+
+    //     pins.setPull(dht11_pin, PinPullMode.PullUp)
+    //     pins.digitalWritePin(dht11_pin, 0) //begin protocol, pull down pin
+    //     basic.pause(18)
+    //     pins.digitalReadPin(dht11_pin) //pull up pin
+    //     control.waitMicros(40)
+    //     last_time = control.micros()
+    //     while (pins.digitalReadPin(dht11_pin) == 0 && control.micros() - last_time < 100); //sensor response
+    //     last_time = control.micros()
+    //     while (pins.digitalReadPin(dht11_pin) == 1 && control.micros() - last_time < 100); //sensor response
+
+    //     //read data (5 bytes)
+    //     for (let index = 0; index < 40; index++) {
+    //         last_time = control.micros()
+    //         while (pins.digitalReadPin(dht11_pin) == 1 && control.micros() - last_time < 80);
+    //         last_time = control.micros()
+    //         while (pins.digitalReadPin(dht11_pin) == 0 && control.micros() - last_time < 60);
+    //         control.waitMicros(28)
+    //         //if sensor still pull up data pin after 28 us it means 1, otherwise 0
+    //         if (pins.digitalReadPin(dht11_pin) == 1) dataArray[index] = true
+    //     }
+    //     //convert byte number array to integer
+    //     for (let index = 0; index < 5; index++)
+    //         for (let index2 = 0; index2 < 8; index2++)
+    //             if (dataArray[8 * index + index2]) dht11_resultArray[index] += 2 ** (7 - index2)
+    //     //verify checksum
+    //     checksumTmp = dht11_resultArray[0] + dht11_resultArray[1] + dht11_resultArray[2] + dht11_resultArray[3]
+    //     checksum = dht11_resultArray[4]
+    //     if (checksumTmp >= 512) checksumTmp -= 512
+    //     if (checksumTmp >= 256) checksumTmp -= 256
+
+    // }
+
+    loops.everyInterval(2000,function(){
+        if (dht11_pin == 0) {
             return
         }
 
         let checksum: number = 0
         let checksumTmp: number = 0
         let dataArray: boolean[] = []
-        let last_time:number = 0
+        let last_time: number = 0
 
         for (let index = 0; index < 40; index++) dataArray.push(false)
         for (let index = 0; index < 5; index++) dht11_resultArray.push(0)
@@ -483,10 +528,7 @@ namespace Environment {
         checksum = dht11_resultArray[4]
         if (checksumTmp >= 512) checksumTmp -= 512
         if (checksumTmp >= 256) checksumTmp -= 256
-
-    }
-
-    loops.everyInterval(2000,get_dht11value())
+    })
 
     /**
      * get dht11 temperature and humidity Value
