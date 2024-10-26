f54dfd1ea158251f2fe8af418ff77820e975b832
zhuyu <zhuyu@elecfreaks.com>
2024-10-26 12:08:30 +0800

    nomsg

--- environment.ts
+++ environment.ts
@@ -77,6 +77,8 @@ namespace Environment {
     const digH6 = 0xE7
 
     let Reference_VOLTAGE = 3100
+    let PH_value_cnt = 0
+    let PH_value:number [] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 
     export enum DHT11Type {
         //% block="temperature(℃)" enumval=0
@@ -857,11 +859,12 @@ namespace Environment {
     //% blockId="readPHLevel" block="PH sensor %Rjpin level(0~14)"
     export function readPHLevel(pin: AnalogPin): number {
         let PHlevel = 0.0;
-        for(let i = 0; i < 100; i++){
-            PHlevel += pins.analogReadPin(pin);
-            basic.pause(1);
+        if (PH_value_cnt >= 10) PH_value_cnt = 0;
+        PH_value[PH_value_cnt++] = pins.analogReadPin(pin)
+        for(let i = 0; i < 10; i++){
+            PHlevel +=  PH_value[i];
         }
-        PHlevel = PHlevel / 100.0
+        PHlevel = PHlevel / 10.0
         PHlevel = 3.3 * (PHlevel/ 1023.0)
         PHlevel = (PHlevel * (-5.7541) + 16.654) * compensation_factor
         //直线斜率优化
